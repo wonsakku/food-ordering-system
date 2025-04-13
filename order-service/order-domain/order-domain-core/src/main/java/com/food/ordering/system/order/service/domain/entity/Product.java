a421078687557ca2011c31f3868a7428026aa849
wonsakku <kws1044@gmail.com>
2025-04-13 19:08:35 +0900

    3.14 Implementing Order Service Domain Logic using DDD

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
@@ -0,0 +1,26 @@
+package com.food.ordering.system.order.service.domain.entity;
+
+import com.food.ordering.system.domain.entity.BaseEntity;
+import com.food.ordering.system.domain.valueobject.Money;
+import com.food.ordering.system.domain.valueobject.ProductId;
+
+// Entity는 고유식별자로 구분하기 때문에 Product에서 따로 EqualsAndHashCode를 정의할 필요 없음.
+// BaseEntity에서 정의
+public class Product extends BaseEntity<ProductId> {
+    private String name;
+    private Money price;
+
+    public Product(ProductId productId, String name, Money price) {
+        super.setId(productId);
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+}
