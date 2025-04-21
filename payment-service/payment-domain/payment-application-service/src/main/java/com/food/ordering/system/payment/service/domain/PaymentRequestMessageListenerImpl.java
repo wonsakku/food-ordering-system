15853215f8d7f0cd0800e61f4c5253f5c8838f6e
wonsakku <kws1044@gmail.com>
2025-04-21 13:40:47 +0900

    6.48. Application Service domain module: Implementing input ports - Part 1

---
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/PaymentRequestMessageListenerImpl.java
@@ -0,0 +1,29 @@
+package com.food.ordering.system.payment.service.domain;
+
+import com.food.ordering.system.payment.service.domain.dto.PaymentRequest;
+import com.food.ordering.system.payment.service.domain.event.PaymentEvent;
+import com.food.ordering.system.payment.service.domain.ports.input.message.listener.PaymentRequestMessageListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class PaymentRequestMessageListenerImpl implements PaymentRequestMessageListener {
+
+    private final PaymentRequestHelper paymentRequestHelper;
+
+    public PaymentRequestMessageListenerImpl(PaymentRequestHelper paymentRequestHelper) {
+        this.paymentRequestHelper = paymentRequestHelper;
+    }
+
+    @Override
+    public void completePayment(PaymentRequest paymentRequest) {
+        PaymentEvent paymentEvent = paymentRequestHelper.persistPayment(paymentRequest);
+    }
+
+    @Override
+    public void cancelPayment(PaymentRequest paymentRequest) {
+        PaymentEvent paymentEvent = paymentRequestHelper.persistCancelPayment(paymentRequest);
+    }
+
+}
