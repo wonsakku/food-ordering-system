6348997f9851c4c4302ef9260c506c9b060bf0b4
wonsakku <kws1044@gmail.com>
2025-04-21 13:48:33 +0900

    6.49. Application Service domain module: Implementing input ports - Part 2

--- payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/out/publisher/PaymentCancelledMessagePublisher.java
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/out/publisher/PaymentCancelledMessagePublisher.java
@@ -1,7 +1,8 @@
 package com.food.ordering.system.payment.service.domain.ports.out.publisher;
 
 import com.food.ordering.system.domain.event.DomainEvent;
+import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
 import com.food.ordering.system.payment.service.domain.event.PaymentCancelledEvent;
 
-public interface PaymentCancelledMessagePublisher extends DomainEvent<PaymentCancelledEvent> {
+public interface PaymentCancelledMessagePublisher extends DomainEventPublisher<PaymentCancelledEvent> {
 }
