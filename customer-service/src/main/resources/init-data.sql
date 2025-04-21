0ad089c56fa0bde225107a1cfa320574a3d02767
wonsakku <kws1044@gmail.com>
2025-04-21 15:47:35 +0900

    6.54. Implementing Container module

--- customer-service/src/main/resources/init-data.sql
+++ customer-service/src/main/resources/init-data.sql
@@ -1,4 +1,4 @@
 INSERT INTO customer.customers(id, username, first_name, last_name)
 VALUES
-('ed53702f-55ca-48a5-a613-5eecab02c457', 'user_1', 'First', 'USER'),
-('ed53702f-55ca-48a5-a613-5eecab02c458', 'user_2', 'Second', 'USER');
+('d215b5f8-0249-4dc5-89a3-51fd148cfb41', 'user_1', 'First', 'USER'),
+('d215b5f8-0249-4dc5-89a3-51fd148cfb43', 'user_2', 'Second', 'USER');
