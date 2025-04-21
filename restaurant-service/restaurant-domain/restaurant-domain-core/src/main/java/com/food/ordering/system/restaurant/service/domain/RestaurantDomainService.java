c05e4b1b08d0d6537ba9c7270b3f82a97bb277e0
wonsakku <kws1044@gmail.com>
2025-04-21 16:38:50 +0900

    7.56. Domain core module: Adding Exception, Domain events and Domain Service

--- restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/service/domain/RestaurantDomainService.java
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/service/domain/RestaurantDomainService.java
@@ -1,11 +1,18 @@
 package com.food.ordering.system.restaurant.service.domain;
 
+import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
 import com.food.ordering.system.restaurant.service.domain.entity.Restaurant;
 import com.food.ordering.system.restaurant.service.domain.event.OrderApprovalEvent;
+import com.food.ordering.system.restaurant.service.domain.event.OrderApprovedEvent;
+import com.food.ordering.system.restaurant.service.domain.event.OrderRejectedEvent;
 
 import java.util.List;
 
 public interface RestaurantDomainService {
 
-    OrderApprovalEvent validateOrder(Restaurant restaurant, List<String> failureMessages);
+    OrderApprovalEvent validateOrder(Restaurant restaurant,
+                                     List<String> failureMessages,
+                                     DomainEventPublisher<OrderApprovedEvent> approvedEventDomainEventPublisher,
+                                     DomainEventPublisher<OrderRejectedEvent> orderRejectedEventDomainEventPublisher
+    );
 }
--- restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/service/domain/RestaurantDomainServiceImpl.java
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/service/domain/RestaurantDomainServiceImpl.java
@@ -1,5 +1,6 @@
 package com.food.ordering.system.restaurant.service.domain;
 
+import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
 import com.food.ordering.system.domain.valueobject.OrderApprovalStatus;
 import com.food.ordering.system.restaurant.service.domain.entity.Restaurant;
 import com.food.ordering.system.restaurant.service.domain.event.OrderApprovalEvent;
@@ -17,7 +18,10 @@ import static com.food.ordering.system.domain.DomainConstants.UTC;
 public class RestaurantDomainServiceImpl implements RestaurantDomainService {
 
     @Override
-    public OrderApprovalEvent validateOrder(Restaurant restaurant, List<String> failureMessages) {
+    public OrderApprovalEvent validateOrder(Restaurant restaurant,
+                                            List<String> failureMessages,
+                                            DomainEventPublisher<OrderApprovedEvent> approvedEventDomainEventPublisher,
+                                            DomainEventPublisher<OrderRejectedEvent> orderRejectedEventDomainEventPublisher) {
         restaurant.validateOrder(failureMessages);
         log.info("Validating order with id: {}", restaurant.getOrderDetail().getId().getValue());
 
@@ -27,14 +31,18 @@ public class RestaurantDomainServiceImpl implements RestaurantDomainService {
             return new OrderApprovedEvent(restaurant.getOrderApproval(),
                     restaurant.getId(),
                     failureMessages,
-                    ZonedDateTime.now(ZoneId.of(UTC)));
+                    ZonedDateTime.now(ZoneId.of(UTC)),
+                    approvedEventDomainEventPublisher
+                );
         } else {
             log.info("Order is rejected for order id: {}", restaurant.getOrderDetail().getId().getValue());
             restaurant.constructOrderApproval(OrderApprovalStatus.REJECTED);
             return new OrderRejectedEvent(restaurant.getOrderApproval(),
                     restaurant.getId(),
                     failureMessages,
-                    ZonedDateTime.now(ZoneId.of(UTC)));
+                    ZonedDateTime.now(ZoneId.of(UTC)),
+                    orderRejectedEventDomainEventPublisher
+            );
         }
     }
 }
