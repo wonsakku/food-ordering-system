80b63feab42dca4c964d2062b92710f6401e8768
wonsakku <kws1044@gmail.com>
2025-04-21 16:54:53 +0900

    Revert "7.56. Domain core module: Adding Exception, Domain events and Domain Service"
    This reverts commit 19e79c0630221f8390d872f5ab697b263d1072bf.

--- common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/RestaurantOrderStatus.java
+++
@@ -1,6 +0,0 @@
-package com.food.ordering.system.domain.valueobject;
-
-public enum RestaurantOrderStatus {
-    PAID,
-    ;
-}
--- restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/food/ordering/system/restaurant/service/domain/mapper/RestaurantDataMapper.java
+++
@@ -1,8 +0,0 @@
-package com.food.ordering.system.restaurant.service.domain.mapper;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class RestaurantDataMapper {
-
-}
