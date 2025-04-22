5603134b579c2dec2fb24dcc3790586c2c57f764
wonsakku <kws1044@gmail.com>
2025-04-22 13:42:51 +0900

    after-saga

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderPaymentSaga.java
@@ -0,0 +1,54 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.domain.event.EmptyEvent;
+import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
+import com.food.ordering.system.order.service.domain.ports.output.message.publisher.restaurantapproval.OrderPaidRestaurantRequestMessagePublisher;
+import com.food.ordering.system.saga.SagaStep;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Component
+public class OrderPaymentSaga implements SagaStep<PaymentResponse, OrderPaidEvent, EmptyEvent> {
+
+    private final OrderDomainService orderDomainService;
+    private final OrderSagaHelper orderSagaHelper;
+    private final OrderPaidRestaurantRequestMessagePublisher orderPaidRestaurantRequestMessagePublisher;
+
+    public OrderPaymentSaga(OrderDomainService orderDomainService,
+                            OrderSagaHelper orderSagaHelper,
+                            OrderPaidRestaurantRequestMessagePublisher orderPaidRestaurantRequestMessagePublisher) {
+        this.orderDomainService = orderDomainService;
+        this.orderSagaHelper = orderSagaHelper;
+        this.orderPaidRestaurantRequestMessagePublisher = orderPaidRestaurantRequestMessagePublisher;
+    }
+
+
+    @Override
+    @Transactional
+    public OrderPaidEvent process(PaymentResponse paymentResponse) {
+        log.info("Complete payment for order with id : {}", paymentResponse.getOrderId());
+        Order order = orderSagaHelper.findOrder(paymentResponse.getOrderId());
+        OrderPaidEvent domainEvent = orderDomainService.payOrder(order, orderPaidRestaurantRequestMessagePublisher);
+        orderSagaHelper.saveOrder(order);
+        log.info("Order with id: {} is paid", order.getId().getValue());
+        return domainEvent;
+    }
+
+
+    // 결제 실패할 경우 로컬 DB를 롤백만 하면 되니까 EmptyEvent를 반환하면 됨.
+    // 주문 이전 단계에 발생했던 이벤트가 없었기 때문
+    @Override
+    @Transactional
+    public EmptyEvent rollback(PaymentResponse paymentResponse) {
+        log.info("Cancelling order with id: {}", paymentResponse.getOrderId());
+        Order order = orderSagaHelper.findOrder(paymentResponse.getOrderId());
+        orderDomainService.cancelOrder(order, paymentResponse.getFailureMessages());
+        orderSagaHelper.saveOrder(order);
+        log.info("Order with id: {} is cancelled", order.getId().getValue());
+        return EmptyEvent.INSTANCE;
+    }
+}
